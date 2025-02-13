8c422ca043d746afa59cbec8f78e74cb2b9cb8eb
oluwaseyi-1-Ade <154804947+oluwaseyi-1-Ade@users.noreply.github.com>
2025-02-13 12:14:15 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <br />
+  <input type="text" ref="name" />
+  <button @click="handleClick">Click me</button>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal
+      :header="header"
+      text="Modal Content"
+      theme="sale"
+      @close="toggleModal"
+    >
+      <h2>This belongs to the slot</h2>
+      <template v-slot:links>
+        <a href="#">Sign up</a>
+        <a href="#"> Sign in</a>
+      </template>
+    </Modal>
+  </div>
+  <button @click.alt="toggleModal">Open Modal -(alt)</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Modal,
+  },
+  data() {
+    return {
+      title: "My First Vue App :)",
+      header: "Sign up here",
+      showModal: false,
+    };
+  },
+  methods: {
+    handleClick() {
+      this.$refs.name.focus();
+      this.$refs.name.classList.toggle("hello");
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +62,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+      <slot></slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["header", "text", "theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style>
+.modal {
+  width: 450px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+
+.modal h1,
+.modal p {
+  color: #333;
+}
+
+.modal .actions {
+  text-align: center;
+  margin: 30px 0 10px 0;
+  /* background: red; */
+}
+
+.modal .actions a {
+  color: #333;
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+
+.sale {
+  background: red;
+  color: white;
+}
+
+.sale h1,
+.sale p {
+  color: white;
+}
+
+.sale .actions a {
+  color: white;
+}
+</style>
+
+// slots are like an alternative for props, but they are used when passing //
+templates
